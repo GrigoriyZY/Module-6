b4245a911ef97dd75d4f5e47432de744c49f2848
GrigoriyZY <mymail@gzyryanov.ru>
2024-08-22 19:22:26 +0500

    Updated module_6_1.py
    Код скорректирован с учетом замечания о принадлежности метода eat.

--- module_6_1.py
+++ module_6_1.py
@@ -5,7 +5,17 @@ class Animal:
     def __init__(self, name):
         self.alive = True
         self.fed = False
+        self.food = None
         self.name = name
+
+    def eat(self, food):
+        self.food = food
+        if food.edible:
+            print(f'{self.name} съел {food.name}.')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}.')
+            self.alive = False
 
 
 class Plant:
@@ -19,32 +29,12 @@ class Mammal(Animal):
 
     def __init__(self, name):
         super().__init__(name)
-        self.food = None
-
-    def eat(self, food):
-        self.food = food
-        if food.edible:
-            print(f'{self.name} съел {food.name}.')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}.')
-            self.alive = False
 
 
 class Predator(Animal):
 
     def __init__(self, name):
         super().__init__(name)
-        self.food = None
-
-    def eat(self, food):
-        self.food = food
-        if food.edible:
-            print(f'{self.name} съел {food.name}.')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}.')
-            self.alive = False
 
 
 class Flower(Plant):
@@ -71,6 +61,11 @@ print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
 a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
